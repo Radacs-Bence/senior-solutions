f1f79822cc6fc7edd87839a8ba99c30772d6398a
Radacs-Bence <bence.radacs@gmail.com>
2021-06-24 20:29:49 +0200

    "Spring Beanek" tasks

--- locations-spring-solution/src/main/java/locations/LocationsController.java
+++ locations-spring-solution/src/main/java/locations/LocationsController.java
@@ -3,26 +3,20 @@ package locations;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
+
 
 @RestController
 public class LocationsController {
 
-    private List<Location> locations = new ArrayList<>();
+    private LocationsService locationsService;
 
-    public LocationsController() {
-        locations.add(new Location(0L, "a", 1, 1));
-        locations.add(new Location(1L, "b", 1, 1.3));
-        locations.add(new Location(2L, "c", 2, 1.3));
-        locations.add(new Location(3L, "d", 2, 23.4));
+    public LocationsController(LocationsService locationsService) {
+        this.locationsService = locationsService;
     }
 
     @GetMapping("/locations")
-    public List<String> getLocations(){
-        return locations.stream()
-                .map(location -> location.getName())
-                .collect(Collectors.toList());
+    public String getLocations(){
+        return locationsService.getLocations().stream().collect(Collectors.joining(", "));
     }
 }
---
+++ locations-spring-solution/src/main/java/locations/LocationsService.java
@@ -0,0 +1,26 @@
+package locations;
+
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class LocationsService {
+
+    private List<Location> locations = new ArrayList<>();
+
+    public LocationsService() {
+        locations.add(new Location(0L, "a", 1, 1));
+        locations.add(new Location(1L, "b", 1, 1.3));
+        locations.add(new Location(2L, "c", 2, 1.3));
+        locations.add(new Location(3L, "d", 2, 23.4));
+    }
+
+    public List<String> getLocations(){
+        return locations.stream()
+                .map(Location::getName)
+                .collect(Collectors.toList());
+    }
+}
