baffada177c785cf4c559412b20f9cb3a42aa6e2
Radacs-Bence <bence.radacs@gmail.com>
2021-06-24 16:27:33 +0200

    JAVAX-MCR tasks initial commit

---
+++ locations-spring-solution/src/main/java/locationsspringsolution/LocationsSpringSolutionApplication.java
@@ -0,0 +1,13 @@
+package locationsspringsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LocationsSpringSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LocationsSpringSolutionApplication.class, args);
+	}
+
+}
---
+++ locations-spring-solution/src/test/java/locationsspringsolution/LocationsSpringSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package locationsspringsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LocationsSpringSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
