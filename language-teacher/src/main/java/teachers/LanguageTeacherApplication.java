61cd1d008f65bdb20a9934a9e818ac73ef2ec510
Radacs-Bence <bence.radacs@gmail.com>
2021-08-02 22:37:30 +0200

    Vizsgafeladat; nyelvtanár-nyilvántartó; Initial commit

---
+++ language-teacher/src/main/java/teachers/LanguageTeacherApplication.java
@@ -0,0 +1,13 @@
+package teachers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LanguageTeacherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LanguageTeacherApplication.class, args);
+    }
+
+}
---
+++ language-teacher/src/test/java/teachers/LanguageTeacherApplicationTests.java
@@ -0,0 +1,13 @@
+package teachers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LanguageTeacherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
